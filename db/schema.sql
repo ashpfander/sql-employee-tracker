972b5ef208ca1e135750601957bfebc5cc5fce3c
Ashleigh Pfander <ashleighharvey@Ashleighs-MacBook-Pro.local>
2024-02-09 11:46:10 -0500

    added to schema.sql with suggested tables

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(department_id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30 NOT NULL),
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(role_id)
+    ON DELETE SET NULL
+    FOREIGN KEY (manager_id)
+    REFERENCES (employee_id)
+    ON DELETE SET NULL
+);
